3a69c56811e568ed1e081afce94eb43093c73829
João Victor <j.victorbahia@gmail.com>
2023-07-04 22:29:59 -0300

    exercicio estilização

---
+++ React_Fundamentos/fundamentos_react/src/components/Botao/Botao.jsx
@@ -0,0 +1,16 @@
+export default function Botao() {
+
+    return (
+        <button 
+            style={{
+                color: 'white',
+                background: 'black',
+                border: 'none',
+                width: '150px',
+                height: '40px',
+                borderRadius: '10px'
+            }}>
+                Comprar agora
+        </button>
+    )
+}
