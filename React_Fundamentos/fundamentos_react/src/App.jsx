50da1bddc6c19e7587a2b01188da6030e6e65178
João Victor <j.victorbahia@gmail.com>
2023-07-07 12:11:12 -0300

    aplicação do conceito de props

--- React_Fundamentos/fundamentos_react/src/App.jsx
+++ React_Fundamentos/fundamentos_react/src/App.jsx
@@ -1,9 +1,17 @@
 import Cartao from "./components/Cartao"
+import banner1 from "./assets/imagem1.jpg"
+import banner2 from "./assets/imagem2.jpg"
+import banner3 from "./assets/imagem3.jpg"
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Cartao />
-    </div>
+    <>
+        <h1>Exercicio</h1>
+        <Cartao banner={banner1} titulo="Pôster: Star Wars (1977)" />
+        <Cartao banner={banner2} titulo="Pôster: Empire Strikes Back (1980)" />
+        <Cartao banner={banner3} titulo="Pôster: Return of the Jedi (1983)" />
+    </>
   )
 }
+
+export default App
--- React_Fundamentos/fundamentos_react/src/components/Cartao/index.jsx
+++ React_Fundamentos/fundamentos_react/src/components/Cartao/index.jsx
@@ -1,13 +1,12 @@
-import banner from "../../../public/starWarsBanner.jpg"
 import styles from "./Cartao.module.css"
 import Botao from "../Botao"
 
-export default function Card() {
+export default function Card({banner, titulo}) {
   return (
     <div className={styles.container}>
       <img className={styles.poster} src={banner} alt="Star Wars poster" />
       <div>
-        <h2 className={styles.title}>Pôster do filme Star Wars</h2>
+        <h2 className={styles.title}>{titulo}</h2>
         <p className={styles.description}>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
         <Botao className={styles.button}/>
       </div>
