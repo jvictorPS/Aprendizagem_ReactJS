fdb41c31c32238a6313937d9eeaa6c0281bc437b
João Victor <j.victorbahia@gmail.com>
2023-07-08 00:32:00 -0300

    aplicação do useState

--- ExercicioComponentePerfil/PerfilComponente/src/components/BotaoSeguir/index.jsx
+++ ExercicioComponentePerfil/PerfilComponente/src/components/BotaoSeguir/index.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import styles from './seguir.module.css'
 
 export default function BotaoSeguir() {
 
+    const [textoSeguir , setTextoSeguir] = useState('Seguir')
+    
     function seguir() {
-        alert('Você está seguindo este usuario')
+        if(textoSeguir === "Seguir") {
+            setTextoSeguir('Seguindo')
+        } else {
+            setTextoSeguir('Seguir')
+        }
     }
 
     return (
-        <button className={styles.seguir} onClick={seguir}>Seguir</button>
+        <button className={styles.seguir} onClick={seguir}>{textoSeguir}</button>
     )
 }
